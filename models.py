20c6b2652287f58858caf6617c52f47af38e44e1
Silas Barta <sbarta@gmail.com>
2014-12-17 16:16:04 -0800

    Integer lookup for write operations.

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ class WorkingGrid(object):
         self.factor2_size = factor2_size
         self.width = self.factor1_size + self.factor2_size - 1
 
-    def lookup(self, row, column):
+    def lookup_range(self, row, column):
         """Given a zero-indexed row and column position, return the range of picture numbers that corresponded to that position in DFMM, as a tuple of (low, high)
         """
         # width should be factor1_size + factor2_size - 1
@@ -13,5 +13,26 @@ class WorkingGrid(object):
             return None
         if row > self.factor2_size - 1:  # check too far down
             return None
-        low = 10 * self.width * row + 10 * column
+        low = self._offset(row, column)
         return (low, low + 9)
+
+    def write_index(self, digit, row, column):
+        """Given a digit in range 0-9 and a row/col, retun the index of
+the picture to be looked at for writing
+        """
+        if type(digit) not in (int, long) or digit < 0 or digit > 9:
+            raise InvalidDigit(digit)
+        return self._offset(row, column) + digit
+
+    def _offset(self, row, column):
+        """Helper function for finding offset given row and column"""
+        return row * 10 * self.width + column * 10
+    #def grid_carry_lookup(self, column):
+    #
+
+
+class InvalidDigit(Exception):
+
+    def __init__(self, digit):
+        self.message = "Invalid digit {}; must be an integer \
+            in range 0-9.".format(digit)
--- test_all.py
+++ test_all.py
@@ -10,33 +10,43 @@ class TestWorkingGrid(unittest.TestCase):
 
     def test_position_to_range_within_grid(self):
         expected1 = (0, 9)
-        actual1 = self.wg_obj.lookup(0, 0)
+        actual1 = self.wg_obj.lookup_range(0, 0)
         self.assertEqual(expected1, actual1)
 
         expected2 = (10, 19)
-        actual2 = self.wg_obj.lookup(0, 1)
+        actual2 = self.wg_obj.lookup_range(0, 1)
         self.assertEqual(expected2, actual2)
 
         expected3 = (40, 49)
-        actual3 = self.wg_obj.lookup(1, 0)
+        actual3 = self.wg_obj.lookup_range(1, 0)
         self.assertEqual(expected3, actual3)
 
     def test_position_to_range_invalid(self):
         expected1 = None
-        actual1 = self.wg_obj.lookup(1, 5)
+        actual1 = self.wg_obj.lookup_range(1, 5)
         self.assertEqual(expected1, actual1)
 
         expected2 = None
-        actual2 = self.wg_obj.lookup(2, 3)
+        actual2 = self.wg_obj.lookup_range(2, 3)
         self.assertEqual(expected2, actual2)
+
+    def test_write_picture(self):
+        expected1 = 12
+        actual1 = self.wg_obj.write_index(2, 0, 1)
+        self.assertEqual(expected1, actual1)
+
+    def test_write_picture_invalid(self):
+        for digit in [10, -1, "a"]:
+            with self.assertRaises(models.InvalidDigit) as cm:
+                self.wg_obj.write_index(digit, 1, 2)
 
     #def test_position_to_range_grid_carry(self):
     #    expected1 = (80, 89)
-    #    actual1 = self.wg_obj.grid_carry_lookup(0)
+    #    actual1 = self.wg_obj.grid_carry_lookup_range(0)
     #    self.assertEqual(expected1, actual1)
 
     #    expected2 = (90, 99)
-    #    actual2 = self.wg_obj.grid_carry_lookup(1)
+    #    actual2 = self.wg_obj.grid_carry_lookup_range(1)
     #    self.assertEqual(expected2, actual2)
 
 
